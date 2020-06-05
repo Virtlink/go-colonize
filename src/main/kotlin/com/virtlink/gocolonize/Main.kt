5ffb368ec29f9a8affb997ed629529f85eecc367
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2020-06-05 16:27:32 +0200

    Add main

--- src/main/kotlin/com/virtlink/gocolonize/Main.kt
+++ src/main/kotlin/com/virtlink/gocolonize/Main.kt
@@ -3,6 +3,8 @@ package com.virtlink.gocolonize
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.option
+import java.io.*
 
 fun main(args: Array<String>) = FlcCommand()
     .main(args)
@@ -10,8 +12,12 @@ fun main(args: Array<String>) = FlcCommand()
 
 class FlcCommand : CliktCommand(help="Adds semi-colons to a Go file") {
     val input by argument(help="Input file; or - to read from STDIN")
+    val output by option("-o", "--output", help="Output file (default writes to STDOUT)")
 
     override fun run() {
-        println("Hello World!")
+        val inputStream = if (input != "-") BufferedInputStream(FileInputStream(input)) else System.`in`
+        val outputStream = if (output != null) BufferedOutputStream(FileOutputStream(output!!)) else System.out
+
+        Colonizer.colonize(inputStream, outputStream)
     }
 }
